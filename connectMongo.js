c4e5bf68260f0a70604c7466dd1c33a5adaa1725
William freitas rondon <appparaty@gmail.com>
2025-05-16 17:06:29 -0300

    config .env

--- connectMongo.js
+++ connectMongo.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
+        // Adicionar verificação para a URI do MongoDB
+        if (!process.env.MONGODB_CONNECT_URI) {
+            throw new Error('A variável de ambiente MONGODB_CONNECT_URI não está definida');
+        }
+        
+        // Configuração opcional para evitar o aviso de depreciação
+        mongoose.set('strictQuery', false);
+        
         await mongoose.connect(process.env.MONGODB_CONNECT_URI)
         console.log("Connect to MongoDB successfully")
     } catch (error) {
